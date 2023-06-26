ccf3681a52e5de1059404498d5c305658e141eb3
akramBNA <akrambenaoun1993@gmail.com>
2023-06-26 11:57:55 +0100

    From Hex to String

---
+++ HexadecimalToString.js
@@ -0,0 +1,15 @@
+// Description:
+// A Javascipst code that transform a hexadecimal code to string 
+
+// Solution:
+
+function HexDecode(nbr) {
+    var hex = nbr.toString();
+    var result =""
+    for(let i=0;i<hex.length;i=i+2){
+        result += String.fromCharCode(parseInt(hex.substr(i,2),16));
+    }
+    return result
+}
+
+console.log(HexDecode("616b72616d"));
